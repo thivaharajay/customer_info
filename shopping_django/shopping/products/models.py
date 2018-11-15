3ba2be84d5d863c2b4b9951a525e8dcbdbd382a5
vishnupremsanketh <vishnu.premsanketh@gmail.com>
2018-11-16 06:23:44 +0800

    added model customer_details

--- shopping_django/shopping/products/models.py
+++ shopping_django/shopping/products/models.py
@@ -6,3 +6,8 @@ class product_details(models.Model):
     productName = models.TextField(null=True)
     produtID= models.IntegerField(null=True)
     Category=models.TextField(null=True)
+
+class customer_details(models.Model):
+    customerName = models.TextField(null=True)
+    customerAddress = models.TextField(null=True)
+    customerPhoneNumber = models.IntegerField(null=True)
