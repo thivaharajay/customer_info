9172c495ba017946111b2d003c406a5c6b61d2f5
vishnupremsanketh <vishnu.premsanketh@gmail.com>
2018-11-16 23:11:00 +0800

    added view definition addproduct

--- shopping_django/shopping/products/views.py
+++ shopping_django/shopping/products/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import product_details
 
+
 def productView(request):
+    #renders the products.html
+    productObjects=product_details.objects.all()
+    return render(request,'products.html',{'product_details':productObjects})
 
-    return render(request,'products.html')
 
-# Create your views here.
+def addProduct(request):
+   # category=request.POST.get('va')
+    add_prod = product_details(productName=request.POST['productName'],
+                                produtID=request.POST['productID']
+                                )
+    add_prod.save()
+    
+    return HttpResponseRedirect('/products/')
+    #db.connection.close_all()
--- shopping_django/shopping/shopping/urls.py
+++ shopping_django/shopping/shopping/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import productView
+from products.views import productView,addProduct
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',productView),
+    path('addProduct/',addProduct),
 ]
