d99e8159fbf90f71b6d879ee5943bc742e648008
vishnupremsanketh <vishnu.premsanketh@gmail.com>
2018-11-16 22:13:51 +0800

    added demo products template and rendering html

--- shopping_django/shopping/products/views.py
+++ shopping_django/shopping/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import product_details
+
+def productView(request):
+
+    return render(request,'products.html')
 
 # Create your views here.
